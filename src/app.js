b6fe5da827224841573a8ebb0258f7505d3d5654
Marejke Schulz <m.schulz@strictmobile.de>
2017-09-24 23:03:55 +0200

    adds reset button and countdown update and moment

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 
 function Display (props) {
   const {
@@ -13,7 +14,7 @@ function Display (props) {
 }
 
 Display.propTypes = {
-  time: React.PropTypes.number
+  time: React.PropTypes.object
 }
 
 export default class App extends React.Component {
@@ -26,8 +27,16 @@ export default class App extends React.Component {
         minutes: 0,
         seconds: 0
       },
-      active: 'hours'
+      active: 'hours',
+      timeLeft: 0,
+      shutdownTime: null
     }
+
+    this.startShutdown = this.startShutdown.bind(this)
+    this.next = this.next.bind(this)
+    this.previous = this.previous.bind(this)
+    this.reset = this.reset.bind(this)
+    this.updateCountdown = this.updateCountdown.bind(this)
   }
   addTime (newTime) {
     const {
@@ -69,15 +78,52 @@ export default class App extends React.Component {
       })
     }
   }
-  shutdown () {
+  reset () {
+    this.setState({
+      time: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      },
+      active: 'hours',
+      timeLeft: '',
+      shutdownTime: null
+    })
+  }
+
+  startShutdown () {
     const { time } = this.state
     const { hours, minutes, seconds } = time
     const total = hours * 60 * 60 + minutes * 60 + seconds
-    alert(`time left: ${total} seconds`)
+    const shutdownTime = moment().add(total, 'seconds')
+
+    this.setState({
+      shutdownTime
+    })
+
+    console.log(`shutdown in ${total} seconds`)
+    window.requestAnimationFrame(this.updateCountdown)
+  }
+  updateCountdown () {
+    if (!this.state.shutdownTime) {
+      return
+    }
+    const duration = moment.duration(this.state.shutdownTime.diff(moment()))
+    const timeLeft = `${duration.hours()}:${duration.minutes()}:${duration.seconds()}`
+
+    if (this.state.timeLeft !== timeLeft) {
+      this.setState({
+        timeLeft
+      })
+
+      console.log(`time left: ${timeLeft}`)
+    }
+
+    if (this.state.shutdownTime.isAfter(moment())) {
+      window.requestAnimationFrame(this.updateCountdown)
+    }
   }
   render () {
-    this.letzteZeit = Date.now()
-
     const numberButtons = [...Array(10).keys()]
       .map((index) => (
         <div key={index}>
@@ -92,9 +138,13 @@ export default class App extends React.Component {
           {numberButtons}
         </div>
         <div>
-          <button onClick={() => this.previous()}>&lt;</button>
-          <button onClick={() => this.next()}>&gt;</button>
-          <button onClick={() => this.shutdown()}>Start</button>
+          <button onClick={this.previous}>&lt;</button>
+          <button onClick={this.next}>&gt;</button>
+          <button onClick={this.startShutdown}>Start</button>
+          <button onClick={this.reset}>Reset</button>
+        </div>
+        <div>
+          {this.state.timeLeft}
         </div>
       </div>
     )
