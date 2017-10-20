8753a0c6e238d3a82a048ecc23b2cb6887b96852
Marejke Schulz <m.schulz@strictmobile.de>
2017-10-20 15:14:57 +0200

    add electron packager

--- electron.js
+++ electron.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 300, height: 400})
 
   if (process.env.NODE_ENV === 'development') {
     win.loadURL(url.format({
