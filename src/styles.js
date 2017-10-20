6e492ec800ea029110e0537a26f347973468ce16
Marejke Schulz <m.schulz@strictmobile.de>
2017-10-20 16:28:41 +0200

    cleanup by extracting styles and display component from app.js

---
+++ src/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+border-radius: 3px;
+padding: 0.25em 1em;
+margin: 0.5em 1em;
+background: transparent;
+color: palevioletred;
+border: 2px solid palevioletred;
+`
+
+export const Controls = styled.div`
+display: flex;
+justify-content: center;
+`
+
+export const NumberButton = styled.button`
+border-radius: 3px;
+padding: 0.25em 1em;
+margin: 0.5em 1em;
+background: transparent;
+color: palevioletred;
+border: 2px solid palevioletred;
+`
+
+export const Numberpad = styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+`
+
+export const TimeInputDisplay = styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+color: palevioletred;
+`
+
+export const TimeDisplay = styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+color: palevioletred;
+padding-top: 1em;
+font-size: 3em;
+`
